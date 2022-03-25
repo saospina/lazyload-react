0538e212d954ff1be86ada7068e4b090ee7d66b5
Sergio Ospina <sergioospina@Sergios-MacBook-Pro.local>
2022-03-25 17:48:51 -0500

    Including routes in navigation

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 import { Routes, Route, NavLink, Navigate } from "react-router-dom";
 
-import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
+import { routes } from "./routes";
 
 import logo from "../logo.svg";
 
@@ -12,22 +12,27 @@ export const Navigation = () => {
         <nav>
           <img src={logo} alt="logo" />
           <ul>
-            <li>
-              <NavLink to="/lazy1" className={({isActive}) => isActive ? 'nav-active' : '' }>lazy1</NavLink>
-            </li>
-            <li>
-              <NavLink to="/lazy2" className={({isActive}) => isActive ? 'nav-active' : '' }>lazy2</NavLink>
-            </li>
-            <li>
-              <NavLink to="/lazy3" className={({isActive}) => isActive ? 'nav-active' : '' }>lazy3</NavLink>
-            </li>
+            {routes.map((route) => (
+              <li key={route.to}>
+                <NavLink
+                  to={route.to}
+                  className={({ isActive }) => (isActive ? "nav-active" : "")}
+                >
+                  {route.name}
+                </NavLink>
+              </li>
+            ))}
           </ul>
         </nav>
         <Routes>
-          <Route path="/lazy1" element={<LazyPage1 />} />
-          <Route path="/lazy2" element={<LazyPage2 />} />
-          <Route path="/lazy3" element={<LazyPage3 />} />
-          <Route path="/*" element={<Navigate to="/lazy1" replace/>} />
+          {routes.map((route) => (
+            <Route
+              key={route.to}
+              path={route.path}
+              element={<route.Component />}
+            />
+          ))}
+          <Route path="/*" element={<Navigate to={routes[0].to} replace />} />
         </Routes>
       </div>
     </BrowserRouter>
